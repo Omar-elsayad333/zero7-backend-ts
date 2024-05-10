f65648205a3cd59fbb555cb05c81f065f91cd0ed
Omar-elsayad333 <omarelsayad333@gmail.com>
2024-05-10 15:24:15 +0300

    fix deployment

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import socketConfig from './src/config/socket'
 // Services
 import notificationSocket from './src/services/socket.service'
 
-const server = app.listen(process.env.PORT, async () => {
+const server = app.listen(process.env.PORT, () => {
   console.log('connected to db & listening on port', process.env.PORT)
 })
 
